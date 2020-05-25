a1951069dbfdc17f6d260b17e401d934140de7e6
Markus Felten <Markus.Felten@gmx.de>
2020-05-25 15:49:59 +0200

    feat: add github-repository-provider

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,11 @@
 <script>
   import * as style from "./main.css";
+  import { GithubProvider } from "github-repository-provider";
+
+  const config = {};
+  const provider = new GithubProvider(config);
 </script>
+
+<div>
+ Name: {{provider.name}}
+</div>
