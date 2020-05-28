3007ee0ac99ccf280bbe8a72f674e7995f8a510a
Markus Felten <Markus.Felten@gmx.de>
2020-05-28 13:02:59 +0200

    feat: use AggregationProvider

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,22 @@
 <script>
   import * as style from "./main.css";
-  import { BitbucketProvider } from "bitbucket-repository-provider";
-  import { GiteaProvider } from "gitea-repository-provider";
+  import BitbucketProvider from "bitbucket-repository-provider";
+  import GiteaProvider from "gitea-repository-provider";
+  import AggregationProvider from "aggregation-repository-provider";
 
-  const config = {};
-  //const provider = new BitbucketProvider(localStorage);
-  const provider = GiteaProvider.initialize(config,localStorage);
+  const provider = AggregationProvider.initialize(
+    [BitbucketProvider, GiteaProvider],
+    { logLevel: 'trace' },
+    localStorage
+  );
 </script>
 
 <div>Name: {provider.name}</div>
 
 {#await provider.repository('arlac77/sync-test-repository')}
-	<p>...waiting</p>
+  <p>...waiting</p>
 {:then repo}
-	<p>The repo is {repo.name}</p>
+  <p>The repo is {repo.name}</p>
 {:catch error}
-	<p style="color: red">{error.message}</p>
+  <p style="color: red">{error.message}</p>
 {/await}
