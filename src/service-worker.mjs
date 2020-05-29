0d353d2665bab2ccf1e77e6636fae8c2b55b7b03
Markus Felten <Markus.Felten@gmx.de>
2020-05-30 00:39:54 +0200

    fix: cleanup service-worker

--- src/service-worker.mjs
+++ src/service-worker.mjs
@@ -1,6 +1,8 @@
 const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
+const currentCaches = [PRECACHE, RUNTIME];
+
 const PRECACHE_URLS = [
   "images/BitbucketProvider.svg",
   "images/GiteaProvider.svg",
@@ -9,7 +11,7 @@ const PRECACHE_URLS = [
   "bundle.mjs",
   "bundle.css",
   "bundle.svelte.css"
-].map(u => "/" + u);
+];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener("install", event => {
@@ -22,8 +24,7 @@ self.addEventListener("install", event => {
 });
 
 // The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", event => {
-  const currentCaches = [PRECACHE, RUNTIME];
+self.addEventListener("activate", event =>
   event.waitUntil(
     caches
       .keys()
@@ -36,44 +37,10 @@ self.addEventListener("activate", event => {
         )
       )
       .then(() => self.clients.claim())
-  );
-});
+  )
+);
 
-let count = 0;
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener("fetch", event => {
-  if (event.request.url.endsWith("hello")) {
-    count++;
-
-    event.respondWith(
-      new Response(
-        `Hello from your friendly neighbourhood service worker! #${count}`,
-        {
-          headers: { "Content-Type": "text/html" }
-        }
-      )
-    );
-
-    return;
-  }
-
-  if (event.request.url.endsWith("cache")) {
-    caches.open(PRECACHE).then(cache => {
-      cache.keys().then(keys => {
-        event.respondWith(
-          new Response(JSON.stringify([...keys]), {
-            headers: { "Content-Type": "application/json" }
-          })
-        );
-      });
-    });
-
-    return;
-  }
-
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
@@ -82,12 +49,13 @@ self.addEventListener("fetch", event => {
           return cachedResponse;
         }
 
-        return caches.open(RUNTIME).then(cache =>
-          fetch(event.request).then(response =>
-            // Put a copy of the response in the runtime cache.
-            cache.put(event.request, response.clone()).then(() => response)
-          )
-        );
+        return caches
+          .open(RUNTIME)
+          .then(cache =>
+            fetch(event.request).then(response =>
+              cache.put(event.request, response.clone()).then(() => response)
+            )
+          );
       })
     );
   }
