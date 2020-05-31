14d6e7776cdf9315d0ce1fbd08a93a3cef2ae92d
Markus Felten <Markus.Felten@gmx.de>
2020-06-01 00:30:43 +0200

    fix: cleanup

--- src/components/RepositoryGroupCard.svelte
+++ src/components/RepositoryGroupCard.svelte
@@ -7,10 +7,6 @@
 <div class="card">
   <div class="card-content">
     <div class="card-title">{repositoryGroup.displayName}</div>
-    {#if repositoryGroup.description}{repositoryGroup.description}{/if}
-    {#if repositoryGroup.id}{repositoryGroup.id}{/if}
-    <a href={repositoryGroup.url}>Home</a>
-
     <Attributes object={repositoryGroup}/>
   </div>
 </div>
