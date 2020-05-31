bfdf619044fd8964dfee69076306a649ed89c55b
Markus Felten <Markus.Felten@gmx.de>
2020-05-31 16:53:23 +0200

    fix: debug service worker registration

--- src/main.mjs
+++ src/main.mjs
@@ -1,6 +1,18 @@
 import App from "./App.svelte";
 
-navigator.serviceWorker.register("bundle.service-worker.mjs");
+navigator.serviceWorker
+  .register("bundle.service-worker.mjs")
+  .then(registration => {
+    registration.addEventListener("updatefound", () => {
+      const installingWorker = registration.installing;
+      console.log("A new service worker is being installed:", installingWorker);
+
+      if(installingWorker) {
+        installingWorker.onstatechange = (...args) => console.log(...args);
+      }
+     // console.log("Waiting:", registration.waiting);
+    });
+  });
 
 export default new App({
   target: document.body
