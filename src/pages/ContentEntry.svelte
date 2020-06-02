25c036ea6e30238cacbd8fa0b883302cd621bae0
Markus Felten <Markus.Felten@gmx.de>
2020-06-02 12:55:13 +0200

    feat(entry): display actual content

--- src/pages/ContentEntry.svelte
+++ src/pages/ContentEntry.svelte
@@ -2,5 +2,12 @@
     export let entry;
 </script>
 
-<h1>ContentEntry</h1>
-{entry.name}
+<h1>ContentEntry {entry.name}</h1>
+
+{#await entry.getString()}
+	<p>...waiting</p>
+{:then string}
+	{string}
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
