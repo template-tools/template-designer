11a667de3c90a04319bf3ff403c1d3f3a6c45d69
Markus Felten <Markus.Felten@gmx.de>
2020-09-04 21:15:29 +0200

    fix: use About from common

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,27 +1,16 @@
 <script>
+  import { About, SessionDetails } from "svelte-common";
+
   import name from "consts:name";
   import version from "consts:version";
   import description from "consts:description";
-  import base from "consts:base";
 
   import { serviceWorker } from "../main.mjs";
 </script>
 
-<h2>{name}</h2>
-<p>{description}</p>
-<table class="bordered striped hoverable">
-  <tbody>
-    <tr>
-      <td>Version</td>
-      <td>{version}</td>
-    </tr>
-    <tr>
-      <td>Mounted</td>
-      <td>{base}</td>
-    </tr>
-    <tr>
-      <td>Service Worker</td>
-      <td>{$serviceWorker.state}</td>
-    </tr>
-  </tbody>
-</table>
+<About {name} {version} {description}>
+  <tr>
+    <td>Service Worker</td>
+    <td>{$serviceWorker.state}</td>
+  </tr>
+</About>
