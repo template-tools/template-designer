7a16ff4e1ede2822c28e4957097059ecd49b1a46
Markus Felten <Markus.Felten@gmx.de>
2020-09-17 21:32:18 +0200

    fix: make use of ServiceWorkerDetails

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,6 +1,5 @@
 <script>
-  import { About } from "svelte-common";
-
+  import { About, ServiceWorkerDetails } from "svelte-common";
   import name from "consts:name";
   import version from "consts:version";
   import description from "consts:description";
@@ -9,8 +8,5 @@
 </script>
 
 <About {name} {version} {description}>
-  <tr>
-    <td>Service Worker</td>
-    <td colspan="2">{$serviceWorker.state}</td>
-  </tr>
+  <ServiceWorkerDetails serviceWorker={$serviceWorker} />
 </About>
