978b0c7d379ecbc0aabb8f80796015a7cab2b90f
Markus Felten <Markus.Felten@gmx.de>
2020-09-19 02:07:48 +0200

    fix: make use of ApplicationDetails

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,11 +1,16 @@
 <script>
-  import { About, ServiceWorkerDetails } from "svelte-common";
+  import {
+    About,
+    ApplicationDetails,
+    ServiceWorkerDetails
+  } from "svelte-common";
   import name from "consts:name";
   import version from "consts:version";
   import description from "consts:description";
   import { serviceWorker } from "../main.mjs";
 </script>
 
-<About {name} {version} {description}>
+<About>
+  <ApplicationDetails {name} {version} {description} />
   <ServiceWorkerDetails serviceWorker={$serviceWorker} />
 </About>
