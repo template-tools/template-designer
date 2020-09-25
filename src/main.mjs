3e075e48fc03ab70c5e0101071a2068a2fdacbd2
Markus Felten <Markus.Felten@gmx.de>
2020-09-25 20:33:48 +0200

    fix: show ServiceWorkerRegistrationDetails

--- src/main.mjs
+++ src/main.mjs
@@ -1,8 +1,8 @@
 import { initializeServiceWorker } from "svelte-common";
 import App from "./App.svelte";
 
-const { serviceWorker } = initializeServiceWorker("bundle.service-worker.mjs");
-export { serviceWorker };
+const { serviceWorker, serviceWorkerRegistration } = initializeServiceWorker("bundle.service-worker.mjs");
+export { serviceWorker, serviceWorkerRegistration };
 
 export default new App({
   target: document.body
--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -2,15 +2,17 @@
   import {
     About,
     ApplicationDetails,
-    ServiceWorkerDetails
+    ServiceWorkerDetails,
+    ServiceWorkerRegistrationDetails
   } from "svelte-common";
   import name from "consts:name";
   import version from "consts:version";
   import description from "consts:description";
-  import { serviceWorker } from "../main.mjs";
+  import { serviceWorker, serviceWorkerRegistration } from "../main.mjs";
 </script>
 
 <About>
   <ApplicationDetails {name} {version} {description} />
-  <ServiceWorkerDetails serviceWorker={$serviceWorker} />
+  <ServiceWorkerDetails serviceWorker={$serviceWorker}  />
+  <ServiceWorkerRegistrationDetails serviceWorkerRegistration={$serviceWorkerRegistration} />
 </About>
