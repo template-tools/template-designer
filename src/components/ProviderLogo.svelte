beca17259be2b2f6c7ab7c3a113ff9d4eef9e130
Markus Felten <Markus.Felten@gmx.de>
2020-12-02 15:16:16 +0100

    fix: provider icon name = provider name

--- src/components/ProviderLogo.svelte
+++ src/components/ProviderLogo.svelte
@@ -1,10 +1,9 @@
 <script>
-  import { BaseProvider } from "repository-provider";
   export let provider;
 </script>
 
 <img
-  src="images/{provider instanceof BaseProvider ? provider.constructor.name : provider.name}.svg"
+  src="images/{provider.name}.svg"
   width="16"
   height="16"
   alt={provider.name} />
--- src/service-worker-assets.mjs
+++ src/service-worker-assets.mjs
@@ -2,13 +2,14 @@ import version from "consts:version";
 
 export const ASSET_GENERATION = version;
 export const ASSETS = [
-    "images/BitbucketProvider.svg",
-    "images/GiteaProvider.svg",
-    "images/GithubProvider.svg",
+    "images/bitbucket.svg",
+    "images/gitea.svg",
+    "images/github.svg",
+    "images/gitlab.svg",
     "images/git.svg",
     "favicon.ico",
     "index.html",
     "bundle.main.mjs",
-    "bundle.css"
+    "bundle.main.css"
   ];
   
