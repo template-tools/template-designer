9c214029ee74ca51cb7cfc58e9afab323490607f
Markus Felten <Markus.Felten@gmx.de>
2022-06-12 12:58:35 +0200

    use node prefix

--- src/auth.mjs
+++ src/auth.mjs
@@ -3,4 +3,3 @@ export const client_id = "ed012bee-8b62-47ad-aa73-cc3cbd15cb68";
 export const redirect_uri =
   "https://mfelten.dynv6.net/services/component-template-designer/auth/accept";
 export const state = "dalöfskksdfjfkl";
-
